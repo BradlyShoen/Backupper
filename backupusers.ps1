19af9c53e2331ae497118cb33ede8a3ceb67723b
Shoen <bradly.shoen@umt.edu>
2020-06-24 09:27:18 -0600

    Initial implementation. Backs up all profiles excluding the ones listed in user-blacklist.txt. Backs up folders listed in folderstobackup.txt. Backs up to destination in destination.txt

---
+++ backupusers.ps1
@@ -0,0 +1,26 @@
+﻿$currentDate = Get-Date -Format "MM-dd-yyyy"
+
+$userblacklist = Get-Content "user-blacklist.txt"
+
+$destination = Get-Content "destination.txt"
+
+$folder = Get-Content "folderstobackup.txt"
+
+$computername = gc env:computername
+$userprofiles = Get-ChildItem "C:\Users" -directory
+
+$counter = 0
+
+foreach ($userprofile in $userprofiles){
+	if($userblacklist -notcontains $userprofile.Name){
+		$counter += 1
+		foreach ($f in $folder)
+		{
+			$BackupSource = $userprofile.FullName  + "\" + $f
+			$BackupDestination = $destination + "\" + $currentDate + " " + $computername + "\" + $userprofile.Name + "\" + $f
+			Copy-Item -ErrorAction SilentlyContinue -recurse -Path  $BackupSource -Destination $BackupDestination
+		}
+	}
+}
+
+Write-Output "`nBacked up $counter profiles"
